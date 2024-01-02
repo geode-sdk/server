84af80c0f17d4209b81d5bd4e8281803ce545974
Fire <17692105+FireMario211@users.noreply.github.com>
2024-01-02 19:16:48 +0000

    Remove defaults for user_id as requested

--- db/setup.sql
+++ db/setup.sql
@@ -7,13 +7,13 @@ CREATE TABLE mods (
 
 CREATE TABLE mods_access (
     mod_id TEXT NOT NULL,
-    user_id INT NOT NULL DEFAULT 1,
+    user_id INT NOT NULL,
     FOREIGN KEY(mod_id) REFERENCES mods(id),
     FOREIGN KEY(user_id) REFERENCES modders(user_id)
 );
 
 CREATE TABLE modders (
-    user_id INT NOT NULL DEFAULT 1 PRIMARY KEY,
+    user_id INT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL DEFAULT 'Developer',
     about_me TEXT
 );
