8c9c954f1f5827f2ad446b1d652c3d03b7f0658b
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-01-03 00:50:31 +0200

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
-use actix_web::{get, web, App, HttpServer, Responder, ResponseError};
+use actix_web::{get, web, App, HttpServer, Responder, ResponseError, middleware::Logger};
+use log::info;
+use env_logger::Env;
 
 mod endpoints;
 mod types;
@@ -38,20 +40,19 @@ async fn main() -> anyhow::Result<()> {
     // Load .env
     dotenvy::dotenv()?;
 
-    // Set up logger
-    // env_logger::init();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let db_url = dotenvy::var("DATABASE_URL")?;
 
-    // Connect to the index database
     let pool = sqlx::SqlitePool::connect(&db_url).await?;
     let addr = "127.0.0.1";
     let port = dotenvy::var("PORT").map_or(8080, |x: String| x.parse::<u16>().unwrap());
 
-    println!("Starting server on {}:{}", addr, port);
+    info!("Starting server on {}:{}", addr, port);
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(AppData { db: pool.clone() }))
+            .wrap(Logger::default())
             .service(endpoints::mods::index)
             .service(endpoints::mods::get)
             .service(endpoints::mods::create)
