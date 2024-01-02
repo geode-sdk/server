47663d1aa3f4eee64f8dfc1d881883503e1fad56
HJfod <60038575+HJfod@users.noreply.github.com>
2024-01-02 17:39:55 +0200

    idk how any of this works

--- db/setup.sql
+++ db/setup.sql
@@ -1,6 +1,20 @@
 
 CREATE TABLE mods (
-    id text,
-    name text,
-    developer text
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    developer TEXT,
+    download_url TEXT,
+);
+
+CREATE TABLE mods_access (
+    mod_id TEXT,
+    user_id INT,
+    FOREIGN KEY(mod_id) REFERENCES mods(id),
+    FOREIGN KEY(user_id) REFERENCES modders(user_id)
+);
+
+CREATE TABLE modders (
+    user_id INT PRIMARY KEY,
+    name TEXT,
+    about_me TEXT
 );
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,78 @@
-use actix_web::{get, web, App, HttpServer, Responder, Error};
+use std::{fmt::Display, io::Write};
+use futures::StreamExt;
+use actix_web::{get, web, App, HttpServer, Responder, ResponseError, post};
 use serde::Serialize;
 
 struct AppData {
     db: sqlx::SqlitePool,
 }
 
+#[derive(Debug)]
+enum Error {
+    FsError,
+    DbAcquireError,
+    DbError,
+    UploadError(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FsError => write!(f, "server filesystem error"),
+            Self::DbAcquireError => write!(f, "database busy"),
+            Self::DbError => write!(f, "database error"),
+            Self::UploadError(msg) => write!(f, "upload error: {msg}"),
+        }
+    }
+}
+
+impl ResponseError for Error {}
+
 #[derive(Serialize)]
 struct Mod {
-    id: String,
-    name: String,
-    developer: String,
+    id: Option<String>,
+    name: Option<String>,
+    developer: Option<String>,
 }
 
 #[get("/v1/mods")]
 async fn list_mods(data: web::Data<AppData>) -> Result<impl Responder, Error> {
-    let mut pool = data.db.acquire().await?;
+    let mut pool = data.db.acquire().await.or(Err(Error::DbAcquireError))?;
     let mods = sqlx::query_as!(Mod, "SELECT * FROM mods")
         .fetch_all(&mut *pool)
-        .await.map_err(|e| e.into())?;
+        .await.or(Err(Error::DbError))?;
 
     Ok(web::Json(mods))
 }
 
 #[get("/v1/mods/{id}")]
 async fn get_mod_by_id(id: String, data: web::Data<AppData>) -> Result<impl Responder, Error> {
-    let mut pool = data.db.acquire().await.map_err(|e| e.into())?;
+    let mut pool = data.db.acquire().await.or(Err(Error::DbAcquireError))?;
     let res = sqlx::query_as!(Mod, r#"SELECT * FROM mods WHERE id = ?"#, id)
         .fetch_one(&mut *pool)
-        .await.map_err(|e| e.into())?;
-    Ok("")
+        .await.or(Err(Error::DbError))?;
+
+    Ok(web::Json(res))
+}
+
+#[post("/v1/mods/{id}")]
+async fn publish_mod(id: String, data: web::Data<AppData>, mut geode_file: web::Payload) -> Result<impl Responder, Error> {
+    // todo: authenticate
+    let mut file = std::fs::File::open(format!("db/temp_{id}.geode")).or(Err(Error::FsError))?;
+    //                                                   ^ todo: sanitize
+    let mut written = 0usize;
+    while let Some(chunk) = geode_file.next().await {
+        let chunk = chunk.map_err(|e| Error::UploadError(e.to_string()))?;
+        written += chunk.len();
+        if written > 262_144 {
+            return Err(Error::UploadError("file too large".to_string()));
+        }
+        file.write_all(&chunk).or(Err(Error::FsError))?;
+    }
+
+    // todo: load info from geode file and add to database
+
+    Ok(web::Json(None::<()>))
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -48,6 +92,7 @@ async fn main() -> anyhow::Result<()> {
                 .app_data(web::Data::new(AppData { db: pool.clone() }))
                 .service(list_mods)
                 .service(get_mod_by_id)
+                .service(publish_mod)
         })
             .bind(("127.0.0.1", 8080))?
             .run()
