86261ea2f0a49ab1e5504cadb49f2428d0cf5f5a
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-01-09 17:17:06 +0200

    remove doubled drop index in first down migration

--- migrations/20240102213218_first_migration.down.sql
+++ migrations/20240102213218_first_migration.down.sql
@@ -2,8 +2,8 @@
 
 DROP TABLE IF EXISTS mods;
 DROP TABLE IF EXISTS mod_versions;
-DROP INDEX IF EXISTS idx_version_id;
 DROP TABLE IF EXISTS dependencies;
 DROP TABLE IF EXISTS developers;
 DROP TABLE IF EXISTS mods_developers;
+
 DROP INDEX IF EXISTS idx_version_id;
