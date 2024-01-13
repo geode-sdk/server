3cf8db8e3811bc72242d130598c8421479e3269e
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-01-13 21:46:43 +0200

    return json for all errors

--- src/types/api.rs
+++ src/types/api.rs
@@ -1,3 +1,4 @@
+use actix_web::{HttpResponse, web, http::header::ContentType};
 use serde::{Serialize, Deserialize};
 use std::fmt::Display;
 use reqwest::StatusCode;
@@ -30,6 +31,11 @@ impl Display for ApiError {
 }
 
 impl actix_web::ResponseError for ApiError {
+    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        HttpResponse::build(self.status_code())
+            .append_header(ContentType::json())
+            .body(web::Json(self.to_string()).to_string())
+    }
     fn status_code(&self) -> StatusCode {
         match self {
             Self::FilesystemError => StatusCode::INTERNAL_SERVER_ERROR,
