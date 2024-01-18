f656a4c55faa16fd5372d6bc5d2f099f2a52b87c
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-01-18 21:57:59 +0200

    add debug mode

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod types;
 
 pub struct AppData {
     db: sqlx::postgres::PgPool,
+    debug: bool
 }
 
 #[get("/")]
@@ -18,7 +19,6 @@ async fn health() -> Result<impl Responder, ApiError> {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let env_url = dotenvy::var("DATABASE_URL")?;
@@ -27,20 +27,26 @@ async fn main() -> anyhow::Result<()> {
         .connect(&env_url).await?;
     let addr = "127.0.0.1";
     let port = dotenvy::var("PORT").map_or(8080, |x: String| x.parse::<u16>().unwrap());
+    let debug = dotenvy::var("APP_DEBUG").unwrap_or("0".to_string()) == "1";
 
     info!("Starting server on {}:{}", addr, port);
-    HttpServer::new(move || {
+    let server = HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppData { db: pool.clone() }))
+            .app_data(web::Data::new(AppData { db: pool.clone(), debug }))
             .wrap(Logger::default())
             .service(endpoints::mods::index)
             .service(endpoints::mods::get)
             .service(endpoints::mods::create)
             .service(endpoints::mods::update)
             .service(health)
-    })
-        .bind((addr, port))?
-        .run()
-        .await?;
+    }).bind((addr, port))?;
+
+    if debug {
+        info!("Running in debug mode, using 1 thread.");
+        server.workers(1).run().await?;
+    } else {
+        server.run().await?;
+    }
+
     anyhow::Ok(())
 }
