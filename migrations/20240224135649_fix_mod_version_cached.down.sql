7c4d0550fa58b7ed384900eb3406a6d2d19b0291
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-02-24 15:57:48 +0200

    finish up the deploy script

---
+++ migrations/20240224135649_fix_mod_version_cached.down.sql
@@ -0,0 +1,4 @@
+-- Add down migration script here
+
+alter table mod_downloads add column if not exists last_download_cache_refresh timestamptz;
+alter table mod_versions drop column if exists last_download_cache_refresh timestamptz;
