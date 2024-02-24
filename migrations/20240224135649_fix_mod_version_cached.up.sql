4c7c2ba0222cb88d12b99ed3a3457375fccae77f
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-02-24 16:00:50 +0200

    i have 1 braincell left

--- migrations/20240224135649_fix_mod_version_cached.up.sql
+++ migrations/20240224135649_fix_mod_version_cached.up.sql
@@ -1,4 +1,4 @@
 -- Add up migration script here
 
-alter table mod_downloads drop column if exists last_download_cache_refresh timestamptz;
+alter table mod_downloads drop column if exists last_download_cache_refresh;
 alter table mod_versions add column if not exists last_download_cache_refresh timestamptz;
