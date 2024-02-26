5953eb9fe269b2c95821d427877b41d9f036d2f2
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-02-27 00:23:53 +0200

    add routes

--- src/main.rs
+++ src/main.rs
@@ -98,6 +98,8 @@ async fn main() -> anyhow::Result<()> {
             .service(endpoints::auth::github::start_github_login)
             .service(endpoints::developers::add_developer_to_mod)
             .service(endpoints::developers::remove_dev_from_mod)
+            .service(endpoints::developers::delete_tokens)
+            .service(endpoints::developers::update_profile)
             .service(health)
     })
     .bind((addr, port))?;
