661caf55696c34e6ef1a61254746eb40d1c45ec6
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-03-02 23:29:39 +0200

    whoops inital migration is no bueno

--- migrations/20240302201649_create_mod_rejections_table.up.sql
+++ migrations/20240302201649_create_mod_rejections_table.up.sql
@@ -4,11 +4,11 @@ create table mod_rejections(
     id serial primary key,
     version text not null,
     reason text not null,
-    created_at timestamp not null default current_timestamp
+    created_at timestamptz not null default now(),
     mod_id text not null,
     admin_id integer not null,
-    FOREIGN KEY (mod_id) REFERENCES mods(id) ON DELETE CASCADE
-    FOREIGN KEY (admin_id) REFERENCES developer(id) ON DELETE CASCADE
+    FOREIGN KEY (mod_id) REFERENCES mods(id) ON DELETE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES developers(id) ON DELETE CASCADE
 );
 
 create index mod_rejections_mod_id_version_idx on mod_rejections(mod_id, version);
