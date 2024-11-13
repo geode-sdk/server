f90f360cbd0a5774753b6f52643cf96f6e65adb9
Fleeym <61891787+Fleeym@users.noreply.github.com>
2024-11-13 17:27:38 +0200

    feat(tag-details): add the endpoint logic

--- src/endpoints/tags.rs
+++ src/endpoints/tags.rs
@@ -24,7 +24,7 @@ pub async fn index(data: web::Data<AppData>) -> Result<impl Responder, ApiError>
 pub async fn detailed_index(data: web::Data<AppData>) -> Result<impl Responder, ApiError> {
     let mut pool = data.db.acquire().await.or(Err(ApiError::DbAcquireError))?;
 
-    let tags = Tag::get_tags(&mut pool).await?;
+    let tags = Tag::get_detailed_tags(&mut pool).await?;
 
     Ok(web::Json(ApiResponse {
         error: "".to_string(),
