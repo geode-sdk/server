922df22560b9caa83005b91f5bcb7869ef1e715b
Fleeym <61891787+Fleeym@users.noreply.github.com>
2025-01-06 15:50:36 +0200

    chore: update dependencies

--- src/types/api.rs
+++ src/types/api.rs
@@ -1,7 +1,7 @@
 use actix_web::{error::QueryPayloadError, http::header::ContentType, HttpRequest, HttpResponse};
-use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
+use actix_web::http::StatusCode;
 
 #[derive(Serialize, Deserialize)]
 pub struct PaginatedData<T> {
@@ -45,14 +45,6 @@ impl Display for ApiError {
 }
 
 impl actix_web::ResponseError for ApiError {
-    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
-        HttpResponse::build(self.status_code())
-            .append_header(ContentType::json())
-            .json(ApiResponse {
-                error: self.to_string(),
-                payload: "".to_string(),
-            })
-    }
     fn status_code(&self) -> StatusCode {
         match self {
             Self::FilesystemError => StatusCode::INTERNAL_SERVER_ERROR,
@@ -65,6 +57,14 @@ impl actix_web::ResponseError for ApiError {
             Self::Unauthorized => StatusCode::UNAUTHORIZED,
             Self::Forbidden => StatusCode::FORBIDDEN,
         }
+    }
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        HttpResponse::build(self.status_code())
+            .append_header(ContentType::json())
+            .json(ApiResponse {
+                error: self.to_string(),
+                payload: "".to_string(),
+            })
     }
 }
 
