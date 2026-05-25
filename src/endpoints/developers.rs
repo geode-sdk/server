b861e179ff8fa742b156ed0eadbb3d7482ef0199
Chloe <git@celosia.zip>
2026-05-24 21:05:31 -0700

    update bans to support revocation

--- src/endpoints/developers.rs
+++ src/endpoints/developers.rs
@@ -1,6 +1,7 @@
 use actix_web::{HttpResponse, Responder, delete, get, post, put, web};
 use serde::{Deserialize, Serialize};
 use utoipa::{IntoParams, ToSchema};
+use chrono::{DateTime, Utc};
 
 use super::ApiError;
 use crate::config::AppData;
@@ -72,6 +73,7 @@ struct DeveloperIndexQuery {
 #[derive(Deserialize, ToSchema)]
 struct DeveloperBanPayload {
     reason: Option<String>,
+    revoked_at: Option<DateTime<Utc>>,
 }
 
 /// List all developers with optional search and pagination
@@ -547,6 +549,7 @@ pub async fn ban_developer(
         path.id,
         dev.id,
         payload.reason.as_deref(),
+        payload.revoked_at,
         &mut pool,
     )
     .await?;
--- src/types/models/developer.rs
+++ src/types/models/developer.rs
@@ -47,8 +47,10 @@ impl Developer {
 
 #[derive(sqlx::FromRow, Serialize, Clone, Debug, ToSchema)]
 pub struct DeveloperBan {
+    pub id: i32,
     pub developer_id: i32,
     pub reason: Option<String>,
     pub admin_id: Option<i32>,
     pub created_at: DateTime<Utc>,
+    pub revoked_at: Option<DateTime<Utc>>,
 }
