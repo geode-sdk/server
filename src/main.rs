ab1c2c78f20db848220d42d904be1e19ad035f2e
Fleeym <fleeym@proton.me>
2026-07-14 01:11:21 +0300

    fix: use mimalloc when compiling with musl

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,12 @@ mod storage;
 mod types;
 mod webhook;
 
+// Avoid musl's default allocator due to lackluster performance
+// https://nickb.dev/blog/default-musl-allocator-considered-harmful-to-performance
+#[cfg(target_env = "musl")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     logging::init();
